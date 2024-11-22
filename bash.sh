b445590e5e100878f69f017c8dab4ae1e3e04abc
John Woods <jpwpkh@umsystem.edu>
2024-11-21 19:33:16 -0600

    bash and sql scripts

---
+++ bash.sh
@@ -0,0 +1,9 @@
+sudo apt install postgres
+curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo apt install pgadmin4
+sudo apt install npm
+
+psql -f database.sql
+
+npm start
